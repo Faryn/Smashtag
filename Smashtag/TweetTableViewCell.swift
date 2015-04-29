3b836c426413c7ad640b16d444fc6cb6f74ab43e
Paul Pfeiffer <paul@MacBook-Pro-4.home.local>
2015-04-29 23:12:13 +0200

    Initial commit from local git

---
+++ Smashtag/TweetTableViewCell.swift
@@ -0,0 +1,46 @@
+//
+//  TweetTableViewCell.swift
+//  Smashtag
+//
+//  Created by Paul Pfeiffer on 26/04/15.
+//  Copyright (c) 2015 Paul Pfeiffer. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+
+    var tweet: Tweet? {
+        didSet {
+            updateUI()
+        }
+    }
+    
+    @IBOutlet weak var tweetProfileImageView: UIImageView!
+    @IBOutlet weak var tweetScreenNameLabel: UILabel!
+    @IBOutlet weak var tweetTextLabel: UILabel!
+    
+    func updateUI() {
+        tweetTextLabel.attributedText = nil
+        tweetScreenNameLabel?.text = nil
+        tweetProfileImageView?.image = nil
+        //tweetCreatedLabel?.text = nil
+    
+        if let tweet = self.tweet {
+            tweetTextLabel?.text = tweet.text
+            if tweetTextLabel?.text != nil {
+                for _ in tweet.media {
+                    tweetTextLabel.text! += " 📷"
+                }
+            }
+            
+            tweetScreenNameLabel?.text = "\(tweet.user)"
+            
+            if let profileImageURL = tweet.user.profileImageURL {
+                if let imageData = NSData(contentsOfURL: profileImageURL) {
+                    tweetProfileImageView?.image = UIImage(data: imageData)
+                }
+            }
+        }
+    }
+}
